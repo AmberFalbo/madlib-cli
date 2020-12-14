86b2afdab31d8e7c542e0b1a76c950da283db40f
Amber Falbo <amberannfalbo@gmail.com>
2020-12-13 23:22:53 -0800

    try again

---
+++ madlib_cli/madlib.py
@@ -0,0 +1,59 @@
+# first_name = input("What is your first name?  ")
+# print(""" 
+# Hello, {} and welcome! 
+# ********************************************
+# **       This is a madlibs game!!!        **
+# **  add verbs, nouns, adjectives etc...   **
+# **     to make a really fun sentence!     **
+# ******************************************** """.format(first_name))
+# hints = input("""
+# ********************************************
+# ***    Would you like a refressher on    ***
+# ***  adjectives, verbs and nouns first?  ***
+# ***               (yes/no)               ***
+# ******************************************** 
+# >   """)
+# if hints == "yes":
+#     print("""
+
+# ********************************************
+# **   We all need a refresher sometimes!   **
+# **      Here are some helpful hints!      **
+# **             -- Remember --             **
+# **    Whatever you put in will be fun!    **
+# ********************************************    
+
+# Adjectives - a word or phrase naming an attribute, added to or grammatically related to a noun to modify or describe it.
+# --------
+# EXAMPLES
+# fancy, jolly, silly, fierce, scary, little, small
+
+# Past Tense Verbs - The past tense is a verb tense used for a past activity or a past state of being. For example: I jumped in the lake. (This is a past activity.) I was happy.
+# --------
+# EXAMPLES
+# walked, hurried, worried, hoped, petted, had, did, made
+
+# Plural Nouns - (Noun - a word (other than a pronoun) used to identify people, places, or things.) -- To make regular nouns plural, add ‑s to the end.
+# --------
+# EXAMPLES
+#   cats, houses, computers, rooms, buses, wolves, chefs, puppies    
+# --------      
+# """)
+# ready = input("""
+# ********************************************
+# ***     Ready to play some MadLibs??     ***
+# ***               (yes/no)               ***
+# ******************************************** 
+# >    """)
+
+
+
+#import regex
+import re
+
+print("Hello, let's play madlibs just give me some verbs and adjectives when prompted to then see a really fun sentence.")
+
+
+
+
+
